5b20263232ce62ea94b32b29599a21f8ff92b5fe
almarzouk <jumaa.almarzouk@gmail.com>
2023-04-12 16:31:03 +0200

    first upload

---
+++ client/src/pages/Contact.js
@@ -0,0 +1,100 @@
+import React from "react";
+import BreadCrumb from "../components/BreadCrumb";
+import Meta from "../components/Meta";
+import { AiOutlineHome, AiOutlineMail } from "react-icons/ai";
+import { BiPhoneCall, BiInfoCircle } from "react-icons/bi";
+import Container from "../components/Container";
+
+const Contact = () => {
+  return (
+    <>
+      <Meta title={"Contact Us"} />
+      <BreadCrumb title="Contact Us" />
+      <Container class1="contact-wrapper py-5 home-wrapper-2">
+        <div className="row">
+          <div className="col-12">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d155421.59372138305!2d13.564813952106467!3d52.506970379497524!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a84e373f035901%3A0x42120465b5e3b70!2z2KjYsdmE2YrZhg!5e0!3m2!1sar!2sde!4v1681309154663!5m2!1sar!2sde"
+              width="600"
+              height="450"
+              className="border-0 w-100"
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
+          <div className="col-12 mt-5">
+            <div className="contact-inner-wrapper d-flex justify-content-between ">
+              <div>
+                <h3 className="contact-title mb-4">Contact</h3>
+                <form action="" className="d-flex flex-column gap-15">
+                  <div>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Name"
+                    />
+                  </div>
+                  <div>
+                    <input
+                      type="email"
+                      className="form-control"
+                      placeholder="Email"
+                    />
+                  </div>
+                  <div>
+                    <input
+                      type="tel"
+                      className="form-control"
+                      placeholder="Mobile Number"
+                    />
+                  </div>
+                  <div>
+                    <textarea
+                      name=""
+                      id=""
+                      className="w-100 form-control"
+                      cols="30"
+                      rows="4"
+                      placeholder="Comments"
+                    ></textarea>
+                  </div>
+                  <div>
+                    <button className="button border-0">Submit</button>
+                  </div>
+                </form>
+              </div>
+              <div>
+                <h3 className="contact-title mb-4">Get in touch with us</h3>
+                <div>
+                  <ul className="ps-0">
+                    <li className="mb-3 d-flex gap-15 align-items-center">
+                      <AiOutlineHome className="fs-5" />
+                      <address className="mb-0">Berlin, Germany 12021</address>
+                    </li>
+                    <li className="mb-3 d-flex gap-15 align-items-center">
+                      <BiPhoneCall className="fs-5" />
+                      <a href="tel:+33 0766763306">+33 0766763306</a>
+                    </li>
+                    <li className="mb-3 d-flex gap-15 align-items-center">
+                      <AiOutlineMail className="fs-5" />
+                      <a href="mailto:jumaa.almarzouk@gmail.com">
+                        jumaa.almarzouk@gmail.com
+                      </a>
+                    </li>
+                    <li className="mb-3 d-flex gap-15 align-items-center">
+                      <BiInfoCircle className="fs-5" />
+                      <p className="mb-0">Monday – Friday 10 AM – 8 PM</p>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export default Contact;
